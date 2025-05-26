8b9da70c6e327c01b6cb72fd28be2e54773941f3
Nicolas Ribeiro Campos <nicolasribeirocampos@MacBook-Air-de-Nicolas.local>
2025-05-26 19:32:14 -0300

    Alteração na rota debug.html e main.py

--- main.py
+++ main.py
@@ -153,10 +153,11 @@ def atualizar(id):
 
     return render_template("atualizar.html", receita=receita)
 
-@app.route("/debug-dados")
+@app.route("/debug-dados", methods=["GET", "POST"])
 def debug_dados():
     receitas = Receita.query.all()
     return render_template("debug.html", receitas=receitas)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
