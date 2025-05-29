1dbaecfd618563a3674f28e80d51b6f0499538d6
Nicolas Ribeiro Campos <nicolasribeirocampos@MacBook-Air-de-Nicolas.local>
2025-05-29 19:51:55 -0300

    Rota debug

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def atualizar(id):
 
     return render_template("atualizar.html", receita=receita)
 
-@app.route("/debug-dados")
+@app.route("/debug")
 def debug_dados():
     receitas = Receita.query.all()
     return render_template("debug.html", receitas=receitas)
